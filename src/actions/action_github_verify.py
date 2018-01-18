0c32a001bdf1e5c13cad4eb35f426c03fee9fbb7
Viktor Adam <rycus86@gmail.com>
2018-01-18 00:14:52 +0000

    Fix GitHub webhook verification on Python 3

--- src/actions/action_github_verify.py
+++ src/actions/action_github_verify.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import six
 import hmac
 from hashlib import sha1
 from flask import request
@@ -15,7 +16,9 @@ class GitHubVerifyAction(Action):
 
     def _run(self):
         # based on https://github.com/carlos-jenkins/python-github-webhooks/blob/master/webhooks.py
-        secret = self._render_with_template(self.secret)
+        secret = str(self._render_with_template(self.secret))
+        if six.PY3:
+            secret = secret.encode('utf-8')
 
         header_signature = request.headers.get('X-Hub-Signature')
         if header_signature is None:
@@ -25,7 +28,7 @@ class GitHubVerifyAction(Action):
         if sha_name != 'sha1':
             self.error('Invalid hashing algorithm')
 
-        mac = hmac.new(str(secret), msg=request.data, digestmod=sha1)
+        mac = hmac.new(secret, msg=request.data, digestmod=sha1)
 
         if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
             self.error('GitHub webhook validation failed')
